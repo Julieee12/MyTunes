4c7ecbcc9d4d468088695d950737c41375e79efd
Julieee12 <anaiuliaopris96@gmail.com>
2023-11-24 13:28:36 +0100

    New/Edit song FXML
    The category field (drop down menu) still needs some work since its pretty complicated to set up. Otherwise I think its complete.

---
+++ MyTunes/src/gui/NewSong.java
@@ -0,0 +1,30 @@
+package gui;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class NewSong {
+    public TextField titlefield;
+    public TextField artistfield;
+    public TextField timefield;
+    public TextField filefield;
+    public Button choose;
+    public Button save;
+    public Button cancel;
+    public Button more;
+
+
+    public void choose(){
+
+    }
+    public void save(){
+
+    }
+    public void cancel(){
+
+    }
+    public void more(){
+
+    }
+
+}
