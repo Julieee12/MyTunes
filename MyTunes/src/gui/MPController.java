3b1ae4b0853a388e130c204ef6a1fecf28accd98
Meile5 <144432344+Meile5@users.noreply.github.com>
2023-11-24 19:13:17 +0100

    Add files via upload
    Made changes to main UI

--- MyTunes/src/gui/MPController.java
+++ MyTunes/src/gui/MPController.java
@@ -1,17 +1,33 @@
 package gui;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class MPController {
 
     public Button prevsong;
-    public Button pause;
     public Button nextsong;
-    public Label songdisplaylbl;
     public Button newsong;
     public ListView songlist;
+    public Button playPause;
+    public ListView allPlaylists;
+    public Label currentlyPlayingSong;
+    public ListView songsInPlaylist;
+    public TextField searchInput;
+    public Button newPlaylist;
+    public Button deletePlaylistID;
+    public Button deleteSongPlaylistID;
+    public Button deleteSongsID;
+    public Button close;
+    public Button moveSongs;
+    public Button searchbutton;
+    public Button moveUp;
+    public Button moveDown;
+    public Button editPlaylist;
+    public Button editSong;
 
     private int currentSong;
 
@@ -29,6 +45,48 @@ public class MPController {
     }
 
 
+    public void playPreviousSong(ActionEvent actionEvent) {
+    }
 
+    public void playSongPause(ActionEvent actionEvent) {
+    }
 
+    public void platNextSong(ActionEvent actionEvent) {
+    }
+
+    public void addNewSong(ActionEvent actionEvent) {
+    }
+
+    public void addNewPlaylist(ActionEvent actionEvent) {
+    }
+
+    public void deletePlaylist(ActionEvent actionEvent) {
+    }
+
+    public void deleteSongInPlaylist(ActionEvent actionEvent) {
+    }
+
+    public void deleteSongs(ActionEvent actionEvent) {
+    }
+
+    public void closeApplication(ActionEvent actionEvent) {
+    }
+
+    public void moveSongsToPlaylists(ActionEvent actionEvent) {
+    }
+
+    public void searchSong(ActionEvent actionEvent) {
+    }
+
+    public void moveSongsUp(ActionEvent actionEvent) {
+    }
+
+    public void moveSongsDown(ActionEvent actionEvent) {
+    }
+
+    public void editExistingPlaylist(ActionEvent actionEvent) {
+    }
+
+    public void editExistingSong(ActionEvent actionEvent) {
+    }
 }
